be16b1b074080ab3389312a0cc289506674f6b08
Gabriela de Andrade <166560478+gabrieladeandradecode@users.noreply.github.com>
2024-06-06 14:42:55 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,122 @@
+function clickChanelTendre(){
+    document.getElementById("p1").innerHTML = "LANÇAMENTO - Uma interpretação ainda mais especial de CHANCE. Uma presença sutil, um rastro intensamente feminino e envolvente."
+}
+
+function clickChanelFraiche(){
+    document.getElementById("p2").innerHTML = "LANÇAMENTO - Fragrância floral amadeirada. Uma intensa explosão de energia e vitalidade. Um encontro olfativo fresco e intenso com a felicidade."
+}
+
+function clickChanelVive(){
+    document.getElementById("p3").innerHTML = "LANÇAMENTO - Uma fragrância floral-especiada em frasco redondo. Uma onda de energia que o levará a um turbilhão de alegria, cheio de bom humor diante do acaso."
+}
+
+function clickChanelChance(){
+    document.getElementById("p4").innerHTML = "LANÇAMENTO - Uma fragrância floral em frasco de linhas arredondadas. Imprevisível, sempre em movimento, CHANCE leva você a um turbilhão de felicidade e fantasia."
+}
+
+function clickChanelN5(){
+    document.getElementById("p5").innerHTML = "N°5, a verdadeira essência da feminilidade. Um buquê floral de aldeídos, um perfume atemporal e lendário."
+}
+
+function clickChanelCoco(){
+    document.getElementById("p6").innerHTML = "A essência de uma mulher ousada e livre.  mbar feminino com personalidade forte e frescor surpreendente."
+}
+
+function clickCliniqueBalsamo(){
+    document.getElementById("p1").innerHTML = "Derrete e remove a maquiagem mais pesada do rosto, lábios e olhos, protetor solar e outras impurezas."
+}
+
+function clickCliniqueHidratante(){
+    document.getElementById("p2").innerHTML = "Hidrata e preenche a pele com Ácido Hialurônico sem deixar a pele oleosa."
+}
+
+function clickCliniqueKit(){
+    document.getElementById("p3").innerHTML = "LANÇAMENTO - Um trio de favoritos de hidratação para te manter hidratada e protegida."
+}
+
+function clickCliniqueBatom(){
+    document.getElementById("p4").innerHTML = "Batom de tom neutro com vitamina E em sua fórmula que hidrata e condiciona, durando até 8 horas."
+}
+
+function clickCliniqueSerum(){
+    document.getElementById("p5").innerHTML = "Sérum Clínico, foca na regularização do tom de pele e na melhora visual do escurecimento e cicatrizes da acne."
+}
+
+function clickCliniquePrimer(){
+    document.getElementById("p6").innerHTML = "Com vitamina C e ácido hialurônico, melhora a aparência da maquiagem, além de hidratar e iluminar a pele."
+}
+
+function esteeSerum(){
+    document.getElementById("p1").innerHTML = "Reduz os sinais de envelhecimento."
+}
+
+function esteeBase(){
+    document.getElementById("p2").innerHTML = "Suaviza, ilumina, refresca e hidrata a pele. Dura 8 horas e deixa a pele lisa em 1 semana."
+}
+
+function esteeBalm(){
+    document.getElementById("p3").innerHTML = "Serve para limpeza diária e remoção de maquiagem de forma suave."
+}
+
+function esteePo(){
+    document.getElementById("p4").innerHTML = "Base e pó de fixação de multiusos, dura até 12 horas."
+}
+
+function esteeCreme(){
+    document.getElementById("p5").innerHTML = "Melhor Creme-Gel que trata olheiras, hidrata, combate radicais livres e reduz sinais de envelhecimento."
+}
+
+function esteePaleta(){
+    document.getElementById("p6").innerHTML = "Paleta multifuncional que contém 10 cores de sombra, 3 tons de batom, 1 tom de blush e 1 tom de iluminador."
+}
+
+function lancomeVie(){
+    document.getElementById("p1").innerHTML = "Fragância feminina icônica da Lâncome, enriquecida com íris, é como um buquê floral gourmet envolto em um sorriso cristalino, que expressa uma feminilidade radiante.  "
+}
+
+function lancomeBase(){
+    document.getElementById("p2").innerHTML = "LANÇAMENTO - Nova tecnologia de brilho natural e saudável que dura 24 horas, com ácido hialurônico e ácido mandélico em sua composição."
+}
+
+function lancomeBatom(){
+    document.getElementById("p3").innerHTML = "Acabamento mate aveludado, de conforto duradouro e cor intensa e viva."
+}
+
+function lancomePaleta(){
+    document.getElementById("p4").innerHTML = "5 tons, sendo 14 harmonias de sombras diferentes e vários looks diferentes."
+}
+
+function lancomeProduto(){
+    document.getElementById("p5").innerHTML = "Dose tripla de cuidado radical com ácido hialurônico, niacinamida C + e ácido ferúlico, um concentrado anti envelhecimento altamente eficaz que restaura a pele camada por camada."
+}
+
+function lancomeCreme(){
+    document.getElementById("p6").innerHTML = "Corrige rugas, reduz manchas, ilumina a pele e a deixa menos flácida."
+}
+
+function lorealShampoo(){
+    document.getElementById("p1").innerHTML = "Enriquecido com Cicatri-Ceramida, que neutraliza os danos desde os primeiros sinais. Sua composição ajuda a reduzir a quebra dos fios, confere mais brilho e elasticidade aos cabelos."
+}
+
+
+function lorealCond(){
+    document.getElementById("p2").innerHTML = "Enriquecido com Cicatri-Ceramida, que neutraliza os danos desde os primeiros sinais. Trata os 5 principais danos causados ​​pelo dia a dia: fragilidade, rigidez, opacidade, ressecamento e pontas duplas."
+}
+
+
+function lorealSerum(){
+    document.getElementById("p3").innerHTML = "Um sérum com maior concentração de ácido hialurônico, suave e antienvelhecimento, reduzindo 40% das rugas faciais em 1 mês."
+}
+
+function lorealUV(){
+    document.getElementById("p4").innerHTML = "Proteção diária contra raios UV e envelhecimento, previne manchas e rugas."
+}
+
+function lorealCastanho(){
+    document.getElementById("p5").innerHTML = "Coloração Triple Cuidado, garante 100% de cobertura dos cabelos grisalhos, ainda mais com o poder da pró queratina, que faz durar mais tempo."
+}
+
+function lorealLouro(){
+    document.getElementById("p6").innerHTML = "Coloração Triple Cuidado, garante 100% de cobertura dos cabelos grisalhos, ainda mais com o poder da pró queratina, que faz durar mais tempo."
+}
+
